7a1ab653f0e70822322f00d65c44b19bd0c44274
maksimdotcom <ya.maksim0511@gmail.com>
2021-11-02 19:59:23 +0300

    remove hardcoded barbers list

--- app.rb
+++ app.rb
@@ -8,12 +8,18 @@ def is_barber_exists? db, name
 end	
 
 def seed_db db, barbers
+	#@barbers = barbers
 	barbers.each do |barber|
 		if !is_barber_exists? db, barber
 			db.execute 'insert into Barbers (name) values (?)', [barber]
 		end
 	end
 end	
+
+before do
+	db = get_db
+	@barbers = db.execute 'select * from Barbers'
+end
 
 def get_db
 	db = SQLite3::Database.new 'barbershop.db'
@@ -40,7 +46,7 @@ configure do
 			   "id" INTEGER PRIMARY KEY AUTOINCREMENT, 
 			   "name" TEXT
 			   )'
-	seed_db db, ['Jessie Pinkman', 'Walter White', 'Gus Fring']
+	seed_db db, ['Jessie Pinkman', 'Walter White', 'Gus Fring', 'Mike Ehrmantra']
 end	
 
 get '/' do
